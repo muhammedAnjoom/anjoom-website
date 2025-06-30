e62b3913c245ade157647aa2e3b95f07a12f9265
muhammedAnjoom <anjoompa42003@gmail.com>
2025-06-30 10:16:25 +0530

    Refactor Footer and Index layout
    - Adjusted the Footer component's styling for improved responsiveness and spacing.
    - Cleaned up the Index page by removing unnecessary wrapper div around the Footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <footer
-    className="max-w-3xl mx-auto px-6 py-5 text-center text-slate-200 border border-emerald-400/30 shadow-xl backdrop-blur-2xl rounded-t-2xl rounded-b-none"
+    className=" md:w-full md:border-0  py-2 md:py-4 text-center text-slate-200 border border-emerald-400/30 shadow-xl backdrop-blur-2xl rounded-t-2xl rounded-b-none"
     style={{
       background: "linear-gradient(135deg, rgba(52,211,153,0.18) 0%, rgba(30,41,59,0.40) 100%)",
       boxShadow: "0 8px 32px 0 rgba(16, 185, 129, 0.15)",
